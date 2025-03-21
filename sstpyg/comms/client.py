e3bcf407184f6e0b17f215e3c958dfd8f3be0940
Sofía Denner <sofi.denner@gmail.com>
2025-03-21 20:51:14 -0300

    add command to client

--- sstpyg/comms/client.py
+++ sstpyg/comms/client.py
@@ -19,9 +19,20 @@ class ClientHandler:
         return response.text
 
 
+    async def command(self):
+        """
+        Send a POST request to the server.
+        """
+        response = await self.client.post(f"http://{self.server_address}/command")
+        return response.text
+
+
+
 if __name__ == "__main__":
     server_address = sys.argv[1]
     print(f"Received parameter: {server_address}")
 
     client = ClientHandler(server_address=server_address)
     print(asyncio.run(client.ping()))
+    print(asyncio.run(client.command()))
+
